fe715ed20957c71f8158dcf041dc7bd1dc485e10
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-10-21 15:47:24 +0300

    Updates

---
+++ precache-manifest.647dea03536a539a8a3a98ba16a28a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "febad677545b636f9960d2d21f07205a",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "8b3bc0ef6e1767e7bfc2",
+    "url": "/TodoList/static/css/main.db15746d.chunk.css"
+  },
+  {
+    "revision": "f2b3d94834c09721dc6f",
+    "url": "/TodoList/static/js/2.e7f6dd74.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.e7f6dd74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3bc0ef6e1767e7bfc2",
+    "url": "/TodoList/static/js/main.5f947f4c.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
